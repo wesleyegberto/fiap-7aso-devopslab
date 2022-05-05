13078c2f36af749ea487a39a5512bd0447190229
Wesley Egberto <wesleyegberto@gmail.com>
2022-05-05 10:18:29 -0300

    Desafio: Custom Message

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello World, Wesley!")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World from FIAP 7ASO, Wesley!")
