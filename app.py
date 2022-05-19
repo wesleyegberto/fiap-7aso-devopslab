c7daef19baf6811e2a994fa63ae1a60373f654ea
Wesley Egberto <wesleyegberto@gmail.com>
2022-05-18 22:38:21 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Hello World from FIAP 7ASO, Wesley!"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+@app.route('/bug')
+def bad():
+    try:
+        raise TypeError()
+    except TypeError as e:
+        print(e)  
 
 if __name__ == '__main__':
     app.run()
